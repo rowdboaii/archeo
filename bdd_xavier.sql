dcf1fd1f3d34cd405c1ff19181bdcf826ce00c9f
jehu <antoine.hars@etu.utc.fr>
2012-09-16 19:38:04 +0200

    Création de la table Personne.

--- bdd_xavier.sql
+++ bdd_xavier.sql
@@ -2,6 +2,15 @@ CREATE DATABASE bdd_xavier owner jehu;
 
 CREATE TABLE region
 (
-	nom_pkey VARCHAR(50) PRIMARY KEY,
+	nom VARCHAR(50) PRIMARY KEY,
 	pays VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE personne
+(
+	nom VARCHAR(50),
+	prenom VARCHAR(50),
+	nationalite VARCHAR(50) NOT NULL,
+	fonction VARCHAR(50) NOT NULL,
+	CONSTRAINT personne_pkey PRIMARY KEY(nom, prenom)
 );
