345c53d607db1929ed8886985696812be7f00d3b
jehu <antoine.hars@etu.utc.fr>
2012-09-16 20:54:14 +0200

    Création de la table Locus.

--- bdd_xavier.sql
+++ bdd_xavier.sql
@@ -1,29 +1,53 @@
+/* Sujet : Projet de base de données pour des fouilles archéologiques.
+ * Auteur : Xavier Mutt & Antoine Hars
+ * Date : 16/09/2012
+ */
+
+/* Création de la database. */
 CREATE DATABASE bdd_xavier owner jehu;
 
+/* Création de la table Région. */
 CREATE TABLE region
 (
 	nom VARCHAR(50) PRIMARY KEY,
 	pays VARCHAR(50) NOT NULL
 );
 
+/* Création de la table Personne. */
 CREATE TABLE personne
 (
 	nom VARCHAR(50),
 	prenom VARCHAR(50),
 	nationalite VARCHAR(50) NOT NULL,
-	fonction VARCHAR(50) NOT NULL,
+	fonction VARCHAR(50) NOT NULL, -- Changer le VARCHAR en ENUM.
 	CONSTRAINT personne_pkey PRIMARY KEY(nom, prenom)
 );
 
+/* Création de la table Site. */
 CREATE TABLE site
 (
 	nom VARCHAR(50) PRIMARY KEY,
-	region VARCHAR(50) NOTN NULL,
+	region VARCHAR(50) NOT NULL,
 	position_nord FLOAT,
 	position_est FLOAT,
 	altitude FLOAT,
-	trouve_par VARCHAR(100) NOT NULL,
-	fouille_par VARCHAR(100) NOT NULL,
-	type VARCHAR(50) NOT NULL,
+	trouve_par VARCHAR(100) NOT NULL, -- Clé étrangère sur Personne à ajouter.
+	fouille_par VARCHAR(100) NOT NULL, -- Clé étrangère sur Personne à ajouter.
+	type VARCHAR(50) NOT NULL, -- Changer le VARCHAR en ENUM.
 	commentaire VARCHAR(500),
+);
+
+/* Ajout de la clé étrangère de Site.region sur Region.nom. */
+ALTER TABLE site ADD CONSTRAINT region_fk FOREIGN KEY(region) REFERENCES region(nom) MATCH FULL;
+
+/* Création de la table Locus. */
+CREAT TABLE locus
+(
+	nom VARCHAR(50) PRIMARY KEY,
+	type VARCHAR(50) NOT NULL, -- Changer le VARCHAR en ENUM.
+	position_nord FLOAT,
+	position_est FLOAT,
+	altitude FLOAT,
+	trouve_par VARCHAR(50) NOT NULL, -- Clé étrangère sur Personne à ajouter.
+	appartient_a VARCHAR(50) NOT NULL -- Clé étrangère sur Personne à ajouter.
 );
