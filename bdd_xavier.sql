dcc2d6109923f63551facb6c4cc21b1ffc05fb6d
jehu <antoine.hars@etu.utc.fr>
2012-09-16 19:50:55 +0200

    Création de la table Site.

--- bdd_xavier.sql
+++ bdd_xavier.sql
@@ -14,3 +14,16 @@ CREATE TABLE personne
 	fonction VARCHAR(50) NOT NULL,
 	CONSTRAINT personne_pkey PRIMARY KEY(nom, prenom)
 );
+
+CREATE TABLE site
+(
+	nom VARCHAR(50) PRIMARY KEY,
+	region VARCHAR(50) NOTN NULL,
+	position_nord FLOAT,
+	position_est FLOAT,
+	altitude FLOAT,
+	trouve_par VARCHAR(100) NOT NULL,
+	fouille_par VARCHAR(100) NOT NULL,
+	type VARCHAR(50) NOT NULL,
+	commentaire VARCHAR(500),
+);
