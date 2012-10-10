0a266c247f59353eb3ba59fff1a276386cfbc892
jehu <antoine.hars@etu.utc.fr>
2012-10-11 01:03:26 +0200

    Ajout Script pour les suppressions.

--- commit.sh
+++ commit.sh
@@ -5,14 +5,15 @@
 # Opérations pour commiter.
 git fetch upstream &&
 git merge upstream/master &&
-git add README.md mcd_bdd_v1.png bdd.sql commit.sh xamppOn.sh xamppOff.sh html/index.php html/mainPage.php html/patron.php html/includes/menuIn.php html/includes/menuOut.php html/includes/piedPage.php html/inputs/articleIn.php html/inputs/carreIn.php html/inputs/charbonIn.php html/inputs/collectionIn.php html/inputs/decapageIn.php html/inputs/form.htm html/inputs/fouilleIn.php html/inputs/galetIn.php html/inputs/gisementIn.php html/inputs/lieuIn.php html/inputs/locusIn.php html/inputs/objetIn.php html/inputs/osIn.php html/inputs/personneIn.php html/inputs/prospectionIn.php html/inputs/regionIn.php html/inputs/silexIn.php html/inputs/siteIn.php html/styles/style.css &&
-git commit -m 'Ajout Formulaires, Menus et Réorganisation.' &&
+git add README.md mcd_bdd_v1.png bdd.sql commit.sh delete.sh xamppOn.sh xamppOff.sh html/index.php html/mainPage.php html/patron.php html/includes/menuIn.php html/includes/menuOut.php html/includes/piedPage.php html/inputs/articleIn.php html/inputs/carreIn.php html/inputs/charbonIn.php html/inputs/collectionIn.php html/inputs/decapageIn.php html/inputs/form.htm html/inputs/fouilleIn.php html/inputs/galetIn.php html/inputs/gisementIn.php html/inputs/lieuIn.php html/inputs/locusIn.php html/inputs/objetIn.php html/inputs/osIn.php html/inputs/personneIn.php html/inputs/prospectionIn.php html/inputs/regionIn.php html/inputs/silexIn.php html/inputs/siteIn.php html/styles/style.css &&
+git commit -m 'Ajout Script pour les suppressions.' &&
 git push origin master
 
 # README.md
 #	mcd_bdd_v1.png
 #	bdd.sql
 #	commit.sh
+# delete.sh
 #	xamppOn.sh
 #	xamppOff.sh
 #	html/index.php
---
+++ delete.sh
@@ -0,0 +1,30 @@
+# Sujet : Projet de base de données pour des fouilles archéologiques.
+# Auteurs : Xavier Muth & Antoine Hars
+# Fichier : delete.sh
+
+git rm bdd_xavier.sql mcd_bdd_xavier_v1.png html/style.css html/siteIn.htm html/silexIn.htm regionIn.htm prospectionIn.htm personneIn.htm patron.htm osIn.htm objetIn.htm mainPage.htm login.htm locusIn.htm lieuIn.htm index.htm gisementIn.htm galetIn.htm form.htm collectionIn.htm charbonIn.htm articleIn.htm &&
+git commit -m 'Suppression de fichiers inutiles.' &&
+git push origin master
+
+# bdd_xavier.sql
+# mcd_bdd_xavier_v1.png
+# html/style.css
+# html/siteIn.htm
+# html/silexIn.htm
+# regionIn.htm
+# prospectionIn.htm
+# personneIn.htm
+# patron.htm
+# osIn.htm
+# objetIn.htm
+# mainPage.htm
+# login.htm
+# locusIn.htm
+# lieuIn.htm
+# index.htm
+# gisementIn.htm
+# galetIn.htm
+# form.htm
+# collectionIn.htm
+# charbonIn.htm
+# articleIn.htm
