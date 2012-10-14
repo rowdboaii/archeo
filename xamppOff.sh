e0ce411c1819958e4652a75f4f2bb40e779b5a17
jehu <antoine.hars@etu.utc.fr>
2012-10-14 20:33:40 +0200

    Suppression de XAMPP.

--- xamppOff.sh
+++
@@ -1,7 +0,0 @@
-# Sujet : Projet de base de données pour des fouilles archéologiques.
-# Auteurs : Xavier Muth & Antoine Hars
-# Fichier : xamppOff.sh
-
-# Arrêter XAMPP.
-sudo /opt/lampp/lampp stop
-
--- xamppOn.sh
+++
@@ -1,8 +0,0 @@
-# Sujet : Projet de base de données pour des fouilles archéologiques.
-# Auteurs : Xavier Muth & Antoine Hars
-# Fichier : xammpOn.sh
-
-# Démarrer XAMPP.
-sudo /opt/lampp/lampp start &&
-firefox http://localhost
-
