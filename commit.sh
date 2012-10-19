35eea4142691409f714b2913ab9862713da27afb
jehu <antoine.hars@etu.utc.fr>
2012-10-20 00:56:37 +0200

    Ajout gisementOut.

--- commit.sh
+++ commit.sh
@@ -3,8 +3,8 @@
 # Fichier : commit.sh
 
 # Opérations pour commiter.
-git add bdd.sql commit.sh delete.sh mcd_bdd_v1.png mcd_bdd_v2.png README.md apacheOn.sh web/index.php web/mainPage.php web/patron.php web/test.php web/includes/menuIn.php web/includes/menuOut.php web/includes/piedPage.php web/inputs/articleIn.php web/inputs/carreIn.php web/inputs/charbonIn.php web/inputs/collectionIn.php web/inputs/decapageIn.php web/inputs/form.htm web/inputs/fouilleIn.php web/inputs/galetIn.php web/inputs/gisementIn.php web/inputs/lieuIn.php web/inputs/locusIn.php web/inputs/objetIn.php web/inputs/osIn.php web/inputs/personneIn.php web/inputs/prospectionIn.php web/inputs/regionIn.php web/inputs/silexIn.php web/inputs/siteIn.php web/styles/style.css web/includes/connexionBDD.php merge.sh web/outputs/articleOut.php web/outputs/regionOut.php web/outputs/siteOut.php web/outputs/locusOut.php web/outputs/personneOut.php web/outputs/collectionOut.php web/outputs/carreOut.php web/outputs/decapageOut.php web/outputs/fouilleOut.php web/outputs/lieuOut.php web/outputs/prospectionOut.php &&
-git commit -m 'Ajout prospectionOut.' &&
+git add bdd.sql commit.sh delete.sh mcd_bdd_v1.png mcd_bdd_v2.png README.md apacheOn.sh web/index.php web/mainPage.php web/patron.php web/test.php web/includes/menuIn.php web/includes/menuOut.php web/includes/piedPage.php web/inputs/articleIn.php web/inputs/carreIn.php web/inputs/charbonIn.php web/inputs/collectionIn.php web/inputs/decapageIn.php web/inputs/form.htm web/inputs/fouilleIn.php web/inputs/galetIn.php web/inputs/gisementIn.php web/inputs/lieuIn.php web/inputs/locusIn.php web/inputs/objetIn.php web/inputs/osIn.php web/inputs/personneIn.php web/inputs/prospectionIn.php web/inputs/regionIn.php web/inputs/silexIn.php web/inputs/siteIn.php web/styles/style.css web/includes/connexionBDD.php merge.sh web/outputs/articleOut.php web/outputs/regionOut.php web/outputs/siteOut.php web/outputs/locusOut.php web/outputs/personneOut.php web/outputs/collectionOut.php web/outputs/carreOut.php web/outputs/decapageOut.php web/outputs/fouilleOut.php web/outputs/lieuOut.php web/outputs/prospectionOut.php web/outputs/gisementOut.php &&
+git commit -m 'Ajout gisementOut.' &&
 git push origin master
 
 #	apacheOn.sh
@@ -56,6 +56,7 @@ git push origin master
 # web/outputs/regionOut.php
 # web/outputs/siteOut.php
 # web/outputs/lieuOut.php
+# web/outputs/gisementOut.php
 
 #	web/styles/style.css
 	
