41a1ec820b39f06b6f0cd560c0ecc45ac7614a52
jehu <antoine.hars@etu.utc.fr>
2012-10-20 01:06:47 +0200

    Ajout silexOut.

---
+++ web/outputs/silexOut.php
@@ -0,0 +1,110 @@
+<!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
+<!-- Auteur : Xavier Muth & Antoine Hars -->
+<!-- Fichier : silexOut.htm -->
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<!-- En-tête de la page. -->
+		<meta charset = "utf-8" />
+		<link rel = "stylesheet" href = "style.css" />
+		<!-- Dans le cas où le navigateur est une version antérieure à IE9 -->
+		<!--[if lt IE9]>
+			<script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<title>silexOut</title>
+	</head>
+
+	<body>
+	  <!-- Corps de la page. -->
+		<div id = "">
+
+			<!-- Connexion à la base de données. -->
+			<?php include('../includes/connexionBDD.php'); ?>
+			
+			<header>
+				<!-- Header de la page. -->
+				<div id = "">
+
+				</div>
+			</header>
+
+			<nav>
+				<!-- Principaux liens de navigation de la page. -->
+				<div id = "">
+
+				</div>
+			</nav>
+
+			<aside>
+				<!-- Menu latéral spécifique au lien visité. -->
+				<div id = "">
+
+				</div>
+			</aside>
+
+			<section>
+				<!-- Section de page. -->
+				<div id = "">
+						
+					<?php $query = $bdd->query('SELECT *
+																			FROM silex'); ?>
+					
+					<!-- Tableau d'affichage de la table. -->
+					<table>
+						<caption>SILEX</caption>
+						
+						<!-- Entête du tableau. -->
+						<thead>
+							<tr>
+								<th>objet</th>
+								<th>provenance</th>
+								<th>couleur</th>
+							</tr>
+						</thead>
+			
+						<!-- Pied du tableau. -->
+						<tfoot>
+							<tr>
+								<th>objet</th>
+								<th>provenance</th>
+								<th>couleur</th>
+							</tr>
+						</tfoot>
+						
+						<!-- Corps du tableau. -->
+						<tbody>
+						
+							<?php
+								while ($data = $query->fetch()) {
+							?>
+								
+								<tr>
+									<td><?php echo $data['objet']; ?></td>
+									<td><?php echo $data['provenance']; ?></td>
+									<td><?php echo $data['couleur']; ?></td>
+								</tr>
+								
+							<?php
+								}
+								$query->closeCursor();
+							?>
+							
+						</tbody>
+					</table>
+
+
+
+				</div>
+			</section>
+
+			<footer>
+				<!-- Pied de la page. -->
+				<?php include('../includes/piedPage.php'); ?>
+			</footer>
+
+		</div>
+	</body>
+
+</html>
