8dec5f03ed1eb9068339df0a72b8b27484346d95
jehu <antoine.hars@etu.utc.fr>
2012-10-19 23:12:51 +0200

    Ajout script pour merger.

---
+++ merge.sh
@@ -0,0 +1,7 @@
+# Sujet : Projet de base de données pour des fouilles archéologiques.
+# Auteurs : Xavier Muth & Antoine Hars
+# Fichier : merge.sh
+
+# Opérations pour merger.
+git fetch upstream &&
+git merge upstream/master
