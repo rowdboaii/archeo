c6b5cfe9232efbdf04d3f0d7d3d7159b381cb443
jehu <antoine.hars@etu.utc.fr>
2012-10-20 00:44:05 +0200

    Ajout fouilleOut.

---
+++ web/outputs/fouilleOut.php
@@ -0,0 +1,113 @@
+<!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
+<!-- Auteur : Xavier Muth & Antoine Hars -->
+<!-- Fichier : fouilleOut.htm -->
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<!-- En-tête de la page. -->
+		<meta charset = "utf-8" />
+		<link rel = "stylesheet" href = "style.css" />
+		<!-- Dans le cas où le navigateur est une version antérieure à IE9 -->
+		<!--[if lt IE9]>
+			<script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<title>fouilleOut</title>
+	</head>
+
+	<body>
+	  <!-- Corps de la page. -->
+		<div id = "">
+
+			<!-- Connexion à la base de données. -->
+			<?php include('../includes/connexionBDD.php'); ?>
+			
+			<header>
+				<!-- Header de la page. -->
+				<div id = "">
+
+				</div>
+			</header>
+
+			<nav>
+				<!-- Principaux liens de navigation de la page. -->
+				<div id = "">
+
+				</div>
+			</nav>
+
+			<aside>
+				<!-- Menu latéral spécifique au lien visité. -->
+				<div id = "">
+
+				</div>
+			</aside>
+
+			<section>
+				<!-- Section de page. -->
+				<div id = "">
+						
+					<?php $query = $bdd->query('SELECT *
+																			FROM fouille'); ?>
+					
+					<!-- Tableau d'affichage de la table. -->
+					<table>
+						<caption>FOUILLE</caption>
+						
+						<!-- Entête du tableau. -->
+						<thead>
+							<tr>
+								<th>identifiant</th>
+								<th>fouilleur</th>
+								<th>decapage</th>
+								<th>année</th>
+							</tr>
+						</thead>
+			
+						<!-- Pied du tableau. -->
+						<tfoot>
+							<tr>
+								<th>identifiant</th>
+								<th>fouilleur</th>
+								<th>decapage</th>
+								<th>année</th>
+							</tr>
+						</tfoot>
+						
+						<!-- Corps du tableau. -->
+						<tbody>
+						
+							<?php
+								while ($data = $query->fetch()) {
+							?>
+								
+								<tr>
+									<td><?php echo $data['identifiant']; ?></td>
+									<td><?php echo $data['fouilleur']; ?></td>
+									<td><?php echo $data['decapage']; ?></td>
+									<td><?php echo $data['annee']; ?></td>
+								</tr>
+								
+							<?php
+								}
+								$query->closeCursor();
+							?>
+							
+						</tbody>
+					</table>
+
+
+
+				</div>
+			</section>
+
+			<footer>
+				<!-- Pied de la page. -->
+				<?php include('../includes/piedPage.php'); ?>
+			</footer>
+
+		</div>
+	</body>
+
+</html>
