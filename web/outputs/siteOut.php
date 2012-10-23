18914bfbe4fa54e82d3ffce1a807d6d14b15798b
antoine <antoine.hars@etu.utc.fr>
2012-10-23 17:05:35 +0300

    Ajout session à siteOut.php

--- web/outputs/siteOut.php
+++ web/outputs/siteOut.php
@@ -1,6 +1,8 @@
 <!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
 <!-- Auteur : Xavier Muth & Antoine Hars -->
-<!-- Fichier : siteOut.htm -->
+<!-- Fichier : siteOut.php -->
+
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html>
