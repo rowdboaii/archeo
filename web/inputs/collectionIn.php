24a81a1a86ab5fbf7be11170f68b29d16caad5b4
antoine <antoine.hars@etu.utc.fr>
2012-10-23 16:43:07 +0300

    Modification commentaire.

--- web/inputs/collectionIn.php
+++ web/inputs/collectionIn.php
@@ -1,6 +1,6 @@
 <!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
 <!-- Auteur : Xavier Muth & Antoine Hars -->
-<!-- Fichier : collectionIn.htm -->
+<!-- Fichier : collectionIn.php -->
 
 <?php
 	session_start();
