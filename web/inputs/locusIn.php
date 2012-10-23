d6844e496839954c1596293b7dc0a9a044d284b7
antoine <antoine.hars@etu.utc.fr>
2012-10-23 16:40:02 +0300

    Ajout condition pseudo à locusIn.php

--- web/inputs/locusIn.php
+++ web/inputs/locusIn.php
@@ -51,6 +51,7 @@
 				</div>
 			</aside>
 
+			<?php if ($_SESSION['pseudo'] == 'sudo') { ?>
 			<section>
 				<!-- Section de page. -->
 				<div id = "">
@@ -102,6 +103,7 @@
 	
 				</div>
 			</section>
+			<?php } ?>
 
 			<footer>
 			
