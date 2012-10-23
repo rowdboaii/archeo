01154175ca7f95ab7a65d9720d7cc1ec98aff7b7
antoine <antoine.hars@etu.utc.fr>
2012-10-23 16:55:17 +0300

    Ajout session à galetOut.php

--- web/outputs/galetOut.php
+++ web/outputs/galetOut.php
@@ -1,6 +1,8 @@
 <!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
 <!-- Auteur : Xavier Muth & Antoine Hars -->
-<!-- Fichier : galetOut.htm -->
+<!-- Fichier : galetOut.php -->
+
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html>
