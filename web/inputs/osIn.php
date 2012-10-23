e4eb9f34411800a83d091ebdf38a8e6945931388
antoine <antoine.hars@etu.utc.fr>
2012-10-23 16:41:20 +0300

    Ajout condition pseudo à osIn.php

--- web/inputs/osIn.php
+++ web/inputs/osIn.php
@@ -51,6 +51,7 @@
 				</div>
 			</aside>
 
+			<?php if ($_SESSION['pseudo'] == 'sudo') { ?>
 			<section>
 				<!-- Section de page. -->
 				<div id = "">
@@ -95,6 +96,7 @@
 	
 				</div>
 			</section>
+			<?php } ?>
 
 			<footer>
 		
