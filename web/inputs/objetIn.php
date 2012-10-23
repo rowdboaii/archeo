76bc65b951be7f5237583753f24f92a08391349f
antoine <antoine.hars@etu.utc.fr>
2012-10-23 15:24:53 +0300

    Ajout session à objetIn.php

--- web/inputs/objetIn.php
+++ web/inputs/objetIn.php
@@ -1,6 +1,10 @@
 <!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
 <!-- Auteur : Xavier Muth & Antoine Hars -->
-<!-- Fichier : objetIn.htm -->
+<!-- Fichier : objetIn.php -->
+
+<?php
+	session_start();
+?>
 
 <!DOCTYPE html>
 <html>
