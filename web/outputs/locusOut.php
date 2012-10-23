3caa81e10cbb4a2846718cbc40460513a328181b
antoine <antoine.hars@etu.utc.fr>
2012-10-23 16:57:48 +0300

    Ajout session à locusOut.php

--- web/outputs/locusOut.php
+++ web/outputs/locusOut.php
@@ -1,6 +1,8 @@
 <!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
 <!-- Auteur : Xavier Muth & Antoine Hars -->
-<!-- Fichier : locusOut.htm -->
+<!-- Fichier : locusOut.php -->
+
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html>
