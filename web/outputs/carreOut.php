29918bb2996789f3002663e658450b808cc5cc58
antoine <antoine.hars@etu.utc.fr>
2012-10-23 16:50:56 +0300

    Ajout session à carreOut.php

--- web/outputs/carreOut.php
+++ web/outputs/carreOut.php
@@ -1,6 +1,8 @@
 <!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
 <!-- Auteur : Xavier Muth & Antoine Hars -->
-<!-- Fichier : carreOut.htm -->
+<!-- Fichier : carreOut.php -->
+
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html>
