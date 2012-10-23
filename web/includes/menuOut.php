b9eab3316aef4495acd90e481d1f04a2cd873133
antoine <antoine.hars@etu.utc.fr>
2012-10-23 14:34:00 +0300

    Ajout condition pseudo pour accès à Personne.

--- web/includes/menuOut.php
+++ web/includes/menuOut.php
@@ -1,21 +1,23 @@
-<!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
+<!-- Sujet : Projet de base de donnï¿½es pour des fouilles archï¿½ologiques. -->
 <!-- Auteur : Xavier Muth & Antoine Hars -->
 <!-- Fichier : menuOut.php -->
 
 <!-- Menu Pour l'affichage des tables. -->
 <div id = "menuOut">
 	<ul>
-		<li><a href = "../outputs/regionOut.php">Région</a></li>
+		<li><a href = "../outputs/regionOut.php">Rï¿½gion</a></li>
 		<li><a href = "../outputs/lieuOut.php">Lieu</a></li>
 		<li><a href = "../outputs/siteOut.php">Site</a></li>
 		<li><a href = "../outputs/locusOut.php">Locus</a></li>
 		<li><a href = "../outputs/prospectionOut.php">Prospection</a></li>
 		<li><a href = "../outputs/articleOut.php">Article</a></li>
 		<li><a href = "../outputs/gisementOut.php">Gisement</a></li>
-		<li><a href = "../outputs/decapageOut.php">Décapage</a></li>
-		<li><a href = "../outputs/carreOut.php">Carré</a></li>
+		<li><a href = "../outputs/decapageOut.php">DÃ©capage</a></li>
+		<li><a href = "../outputs/carreOut.php">CarrÃ©</a></li>
 		<li><a href = "../outputs/fouilleOut.php">Fouille</a></li>
+		<?php if ($_SESSION['pseudo'] == 'sudo') { ?>
 		<li><a href = "../outputs/personneOut.php">Personne</a></li>
+		<?php } ?>
 		<li><a href = "../outputs/collectionOut.php">Collection</a></li>
 		<li><a href = "../outputs/objetOut.php">Objet</a></li>
 		<li><a href = "../outputs/silexOut.php">Silex</a></li>
