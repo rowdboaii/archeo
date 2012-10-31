74b344b2e1b5924ffba930710165f988c6070332
jehu <antoine.hars@etu.utc.fr>
2012-10-31 22:47:35 +0100

    Ajout Update de Charbon.

---
+++ web/updates/charbonUp.php
@@ -0,0 +1,136 @@
+<!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
+<!-- Auteur : Xavier Muth & Antoine Hars -->
+<!-- Fichier : charbonUp.php -->
+
+<!-- Démarrage de la session pour les identifiants. -->
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<!-- En-tête de la page. -->
+		<meta charset = "utf-8" />
+		<link rel = "stylesheet" href = "style.css" />
+		<!-- Dans le cas où le navigateur est une version antérieure à IE9 -->
+		<!--[if lt IE9]>
+			<script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<title>CharbonUp</title>
+	</head>
+
+	<body>
+	  <!-- Corps de la page. -->
+		<div id = "">
+
+			<header>
+				<!-- Header de la page. -->
+				<div id = "">
+				
+				</div>
+			</header>
+
+			<nav>
+				<!-- Principaux liens de navigation de la page. -->
+				<div id = "">
+				
+					<!-- Menu principal. -->
+					<?php include('../includes/menuMain.php'); ?>
+				
+				</div>
+			</nav>
+			
+			<aside>
+				<!-- Menu latéral spécifique au lien visité. -->
+				<div id = "">
+				
+					<!-- Menu pour les Updates. -->
+					<?php include('../includes/menuUp.php'); ?>
+				
+				</div>
+			</aside>
+
+			<?php if ($_SESSION['pseudo'] == 'sudo') { ?>
+			
+			<section>
+				<!-- Section de page. -->
+				<div id = "">
+
+					<?php
+						/* Connexion à la base de données. */
+						include('../includes/connexionBDD.php');
+
+						/* Récupération des données pour le formulaire. */
+						$query1 = $bdd->prepare('SELECT o.identifiant, o.nom
+																			FROM objet o, objetnature n
+																			WHERE o.nature = n.identifiant
+																			AND n.nature = \'charbon\''
+																		 	);
+						$query2 = $bdd->prepare('SELECT o.nom AS nom_objet, c.datation
+																			FROM charbon c, objet o
+																			WHERE c.objet = o.identifiant'
+																			);
+					?>
+
+					<p>
+						<!-- Formulaire pour l'Update d'un Charbon. -->
+						<form method = "post" action = "../exec/charbonUpdate.php">
+							<p>
+								<label for = "old_objet">Objet</label> : 
+								<select name = "old_objet" id = "old_objet">
+									<option value = "0"></option>
+									<?php
+										$query2->execute();
+										while ($data = $query2->fetch()) {
+											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom_objet'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_objet"> remplacé par</label> : 
+								<select name = "new_objet" id = "new_objet">
+									<option value = "0"></option>
+									<?php
+										$query1->execute();
+										while ($data = $query1->fetch()) {
+											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+										}
+									?>
+								</select><br />
+								<label for = "old_datation">Datation</label> : 
+								<select name = "old_datation" id = "old_datation">
+									<option value = "0"></option>
+									<?php
+										$query2->execute();
+										while ($data = $query2->fetch()) {
+											echo '<option value ="' . $data['identifiant'] . '">' . $data['datation'] . '</option>';
+										}
+									?>
+								</select>
+								<input type = "text" name = "new_datation" id = "new_datation" /><br />
+								<input type = "submit" value = "Envoi" />
+							</p>
+						</form>
+					</p>
+					
+					<?php
+						$query1->closeCursor();
+						$query2->closeCursor();
+						$query3->closeCursor();
+						$query4->closeCursor();
+					?>
+				
+				</div>
+			</section>
+			<?php } ?>
+
+			<footer>
+				
+				<!-- Pied de la page. -->
+				<?php include('../includes/piedPage.php'); ?>
+			
+			</footer>
+
+		</div>
+	</body>
+
+</html>
