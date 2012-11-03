cc1d04adf61b0ee5d60c00054dec4d3a7f48112f
jehu <antoine.hars@etu.utc.fr>
2012-11-03 10:51:28 +0100

    Ajout Update de Fouille et Galet.

---
+++ web/updates/fouilleUp.php
@@ -0,0 +1,170 @@
+<!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
+<!-- Auteur : Xavier Muth & Antoine Hars -->
+<!-- Fichier : fouilleUp.php -->
+
+<!-- Démarrage de la session pour les identifiants. -->
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<!-- En-tête de la page. -->
+		<meta charset = "utf-8" />
+		<link rel = "stylesheet" href = "style.css" />
+		<!-- Dans le cas où le navigateur est une version antérieure à IE9 -->
+		<!--[if lt IE9]>
+			<script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<title>FouilleUp</title>
+	</head>
+
+	<body>
+	  <!-- Corps de la page. -->
+		<div id = "">
+
+			<header>
+				<!-- Header de la page. -->
+				<div id = "">
+				
+				</div>
+			</header>
+
+			<nav>
+				<!-- Principaux liens de navigation de la page. -->
+				<div id = "">
+				
+					<!-- Menu principal. -->
+					<?php include('../includes/menuMain.php'); ?>
+				
+				</div>
+			</nav>
+			
+			<aside>
+				<!-- Menu latéral spécifique au lien visité. -->
+				<div id = "">
+				
+					<!-- Menu pour les Updates. -->
+					<?php include('../includes/menuUp.php'); ?>
+				
+				</div>
+			</aside>
+
+			<?php if ($_SESSION['pseudo'] == 'sudo') { ?>
+			
+			<section>
+				<!-- Section de page. -->
+				<div id = "">
+
+					<?php
+						/* Connexion à la base de données. */
+						include('../includes/connexionBDD.php');
+
+						/* Récupération des données pour le formulaire. */
+						$query1 = $bdd->prepare('SELECT d.identifiant, d.nom
+													FROM decapage d'
+													);
+						$query2 = $bdd->prepare('SELECT p.nom, p.prenom, p.identifiant
+													FROM personne p'
+													);
+						$query3 = $bdd->prepare('SELECT f.identifiant, f.nom, p.prenom, p.nom AS nom_personne, d.nom AS nom_decapage, f.annee
+													FROM fouille f, personne p, decapage d
+													WHERE f.fouilleur = p.identifiant
+													AND f.decapage = d.identifiant'
+													);
+					?>
+
+					<p>
+						<!-- Formulaire pour l'Update d'une Fouille. -->
+						<form method = "post" action = "../exec/fouilleUpdate.php">
+							<p>
+								<label for = "old_nom">Nom</label> : 
+								<select name = "old_nom" id = "old_nom">
+									<option value = "0"></option>
+									<?php
+										$query3->execute();
+										while ($data = $query3->fetch()) {
+											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_nom"> remplacé par</label> : 
+								<input type = "text" name = "new_nom" id = "new_nom"><br />
+								<label for = "old_decapage">Décapage</label> : 
+								<select name = "old_decapage" id = "old_decapage">
+									<option value = "0"></option>
+									<?php
+										$query3->execute();
+										while ($data = $query3->fetch()) {
+											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom_decapage'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "old_decapage">Décapage</label> : 
+								<select name = "old_decapage" id = "old_decapage">
+									<option value = "0"></option>
+									<?php
+										$query1->execute();
+										while ($data = $query1->fetch()) {
+											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+										}
+									?>
+								</select><br />
+								<label for = "old_fouilleur">Fouilleur</label> : 
+								<select name = "old_fouilleur" id = "old_fouilleur">
+									<option value = "0"></option>
+									<?php
+										$query3->execute();
+										while ($data = $query3->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['prenom'] . ' ' . $data['nom_personne'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_fouilleur"> remplacé par</label> : 
+								<select name = "new_fouilleur" id = "new_fouilleur">
+									<option value = "0"></option>
+									<?php
+										$query2->execute();
+										while ($data = $query2->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['prenom'] . ' ' . $data['nom'] . '</option>';
+										}
+									?>
+								</select><br />
+								<label for = "old_annee">Année</label> : 
+								<select name = "old_annee" id = "old_annee">
+									<option value = "0"></option>
+									<?php
+										$query3->execute();
+										while ($data = $query3->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['annee'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_annee"> remplacé par</label> : 
+								<input type = "date" name = "new_annee" id = "new_annee"><br />
+								<input type = "submit" value = "Envoi" />
+							</p>
+						</form>
+					</p>
+					
+					<?php
+						$query1->closeCursor();
+						$query2->closeCursor();
+						$query3->closeCursor();
+					?>
+				
+				</div>
+			</section>
+			<?php } ?>
+
+			<footer>
+				
+				<!-- Pied de la page. -->
+				<?php include('../includes/piedPage.php'); ?>
+			
+			</footer>
+
+		</div>
+	</body>
+
+</html>
---
+++ web/updates/galetUp.php
@@ -0,0 +1,160 @@
+<!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
+<!-- Auteur : Xavier Muth & Antoine Hars -->
+<!-- Fichier : galetUp.php -->
+
+<!-- Démarrage de la session pour les identifiants. -->
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<!-- En-tête de la page. -->
+		<meta charset = "utf-8" />
+		<link rel = "stylesheet" href = "style.css" />
+		<!-- Dans le cas où le navigateur est une version antérieure à IE9 -->
+		<!--[if lt IE9]>
+			<script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<title>GaletUp</title>
+	</head>
+
+	<body>
+	  <!-- Corps de la page. -->
+		<div id = "">
+
+			<header>
+				<!-- Header de la page. -->
+				<div id = "">
+				
+				</div>
+			</header>
+
+			<nav>
+				<!-- Principaux liens de navigation de la page. -->
+				<div id = "">
+				
+					<!-- Menu principal. -->
+					<?php include('../includes/menuMain.php'); ?>
+				
+				</div>
+			</nav>
+			
+			<aside>
+				<!-- Menu latéral spécifique au lien visité. -->
+				<div id = "">
+				
+					<!-- Menu pour les Updates. -->
+					<?php include('../includes/menuUp.php'); ?>
+				
+				</div>
+			</aside>
+
+			<?php if ($_SESSION['pseudo'] == 'sudo') { ?>
+			
+			<section>
+				<!-- Section de page. -->
+				<div id = "">
+
+					<?php
+						/* Connexion à la base de données. */
+						include('../includes/connexionBDD.php');
+
+						/* Récupération des données pour le formulaire. */
+						$query1 = $bdd->prepare('SELECT o.identifiant, o.nom
+													FROM objet o, objetnature n
+													WHERE o.nature = n.identifiant
+													AND n.nature = \'galet\''
+													);
+						$query2 = $bdd->prepare('SELECT identifiant, type
+													FROM galettype'
+													);
+						$query3 = $bdd->prepare('SELECT o.nom AS nom_objet, g.nom, t.type AS nom_type
+													FROM galet g, galettype t, objet o
+													WHERE g.objet = o.identifiant
+													AND g.type = t.identifiant'
+													);
+					?>
+
+					<p>
+						<!-- Formulaire pour l'Update d'un Galet. -->
+						<form method = "post" action = "../exec/galetUpdate.php">
+							<p>
+								<label for = "old_objet">Objet</label> : 
+								<select name = "old_objet" id = "old_objet">
+									<option value = "0"></option>
+									<?php
+										$query3->execute();
+										while ($data = $query3->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom_objet'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_objet"> remplacé par</label> : 
+								<select name = "new_objet" id = "new_objet">
+									<option value = "0"></option>
+									<?php
+										$query1->execute();
+										while ($data = $query1->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+										}
+									?>
+								</select><br />
+								<label for = "old_nom">Nom</label> : 
+								<select name = "old_nom" id = "old_nom">
+									<option value = "0"></option>
+									<?php
+										$query3->execute();
+										while ($data = $query3->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_nom"> remplacé par</label> : 
+								<input type = "text" name = "new_nom" id = "new_nom" /><br />
+								<label for = "old_type">Type</label> : 
+								<select name = "old_type" id = "old_type">
+									<option value = "0"></option>
+									<?php
+										$query3->execute();
+										while ($data = $query3->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom_type'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_type"> remplacé par</label> : 
+								<select name = "new_type" id = "new_type">
+									<option value = "0"></option>
+									<?php
+										$query2->execute();
+										while ($data = $query2->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['type'] . '</option>';
+										}
+									?>
+								</select><br />
+								<input type = "submit" value = "Envoi" />
+							</p>
+						</form>
+					</p>
+					
+					<?php
+						$query1->closeCursor();
+						$query2->closeCursor();
+						$query3->closeCursor();
+					?>
+				
+				</div>
+			</section>
+			<?php } ?>
+
+			<footer>
+				
+				<!-- Pied de la page. -->
+				<?php include('../includes/piedPage.php'); ?>
+			
+			</footer>
+
+		</div>
+	</body>
+
+</html>
