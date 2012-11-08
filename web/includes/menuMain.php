a33251f32aa1928a34a74ecb95f7fbdd21853036
antoine <antoine.hars@etu.utc.fr>
2012-11-08 14:13:33 +0100

    Modification de menuMain.php pour le CSS.

--- web/includes/menuMain.php
+++ web/includes/menuMain.php
@@ -3,21 +3,21 @@
 <!-- Fichier : menuMain.php -->
 
 <!-- Menu principal du site. -->
-<div id = "menuMain">
-	<ul>
-		<li><a href = "../mainPage.php">Accueil</a></li>
-		<li><a href = "../output.php">Lecture</a></li>
+<ul id = "menuMain">
+	<li class = "buttonLeft"><a href = "../mainPage.php">Accueil</a></li>
+	<li class = "buttonLeft"><a href = "../output.php">Lecture</a></li>
 
-		<!-- Vérification du type d'utilisateur. -->
-		<?php if ($_SESSION['pseudo'] == 'sudo') { ?>
-		<li><a href = "../input.php">Écriture</a></li>
-		<li><a href = "../update.php">Modification</a></li>
-		<li><a href = "../delete.php">Suppression</a></li>
-		<li><a href = "../parameter.php">Paramètres</a></li>
-		<?php } ?>
-		
-		<li><a href = "../contact.php">Contacts</a></li>
-		<li><a href = "../deconnect.php">Déconnexion</a></li>
-	</ul>
-<div>
+	<!-- Vérification du type d'utilisateur. -->
+	<?php if ($_SESSION['pseudo'] == 'sudo') { ?>
+
+	<li class = "buttonLeft"><a href = "../input.php">Écriture</a></li>
+	<li class = "buttonLeft"><a href = "../update.php">Modification</a></li>
+	<li class = "buttonLeft"><a href = "../delete.php">Suppression</a></li>
+	<li class = "buttonLeft"><a href = "../parameter.php">Paramètres</a></li>
+	<?php } ?>
+	
+	<li class = "buttonRight"><a href = "../contact.php">Contacts</a></li>
+	<li class = "buttonRight"><a href = "../deconnect.php">Déconnexion</a></li>
+</ul>
+
 
