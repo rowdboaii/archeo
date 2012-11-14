dbc1f9fcdc5895519db78c3d814080591b4fc9df
jehu <antoine.hars@etu.utc.fr>
2012-11-15 00:13:14 +0100

    Ajout des updates des paramètres.

---
+++ web/exec/fonctionUpdate.php
@@ -0,0 +1,85 @@
+﻿<!-- Sujet : Projet de base de données pour des fouilles archéologiques. -->
+<!-- Auteur : Xavier Muth & Antoine Hars -->
+<!-- Fichier : fonctionUpdate.php -->
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<!-- En-tête de la page. -->
+		<meta charset = "utf-8" />
+		<link rel = "stylesheet" href = "style.css" />
+		<!-- Dans le cas où le navigateur est une version antérieure à IE9 -->
+		<!--[if lt IE9]>
+			<script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<title>FonctionUpdate</title>
+	</head>
+
+	<body>
+	  <!-- Corps de la page. -->
+		<div id = "">
+
+			<!-- Connexion à la base de données. -->
+			<?php include('../includes/connexionBDD.php'); ?>
+
+			<header>
+				<!-- Header de la page. -->
+				<div id = "">
+				
+				</div>
+			</header>
+
+			<nav>
+				<!-- Principaux liens de navigation de la page. -->
+				<div id = "">
+				
+				</div>
+			</nav>
+			
+			<aside>
+				<!-- Menu latéral spécifique au lien visité. -->
+				<div id = "">
+				
+				</div>
+			</aside>
+			<section>
+
+				<!-- Section de page. -->
+				<div id = "">	
+	
+					<?php
+						$query = $bdd->prepare('UPDATE fonction
+												SET fonction = :new_fonction
+												WHERE identifiant = :identifiant
+												AND identifiant > 0');
+						$query->execute(array('new_fonction' => $_POST['new_fonction'],
+												'identifiant' => $_POST['identifiant']));
+
+						if (!$query) {
+							die("Erreur dans la modification de champ : " . pg_last_error());
+						}
+						else {
+							echo 'Champ modifié.';
+						}
+					?>
+				
+					<!-- Lien de retour. -->
+					<p>
+						<a href = "../parameters/fonction.php">Revenir</a>
+					</p>
+	
+				</div>
+			</section>
+
+			<footer>
+			
+				<!-- Pied de la page. -->
+				<?php include('../includes/piedPage.php'); ?>
+			
+			</footer>
+
+		</div>
+	</body>
+
+</html>
