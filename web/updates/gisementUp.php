2a73cfc2f4c86f7cc9cf4c78989a3f4608ba4ff4
antoine <antoine.hars@etu.utc.fr>
2012-11-15 17:06:06 +0100

    Modification du form de GisementUp.

--- web/updates/gisementUp.php
+++ web/updates/gisementUp.php
@@ -62,95 +62,139 @@
 
 						/* Récupération des données pour le formulaire. */
 						$query1 = $bdd->prepare('SELECT identifiant, nom
-																			FROM region'
-																		 	);
+										FROM region'
+									 	);
 						$query2 = $bdd->prepare('SELECT g.identifiant, g.nom, r.nom AS nom_region, g.position_nord, g.position_est, g.altitude, g.commentaire
-																			FROM gisement g, region r
-																			WHERE g.region = r.identifiant'
-																			);
+										FROM gisement g, region r
+										WHERE g.region = r.identifiant'
+										);
+					?>
+
+					<p>
+						<!-- Formulaire sur le choix du champ à modifier. -->
+						<form method = "post" action = "gisementUp.php">
+							<p>
+								<label for = "champ">Champ à modifier</label> : 
+								<select name = "champ" id = "champ">
+									<option value = "0"></option>
+									<option value = "nom">nom</option>
+									<option value = "region">région</option>
+									<option value = "position_nord">position nord</option>
+									<option value = "position_est">position est</option>
+									<option value = "altitude">altitude</option>
+								</select><br />
+								<input type = "submit" value = "Envoi" />
+							</p>
+						</form>
+					</p>
+
+					<?php
+						/* Récupération du champ à modifier. */
+						$_SESSION['champ'] = 0;
+						if (isset($_POST['champ'])) {	
+							$_SESSION['champ'] = $_POST['champ'];
+						}
+						
+						/* Affichage du champ souhaité. */
+						if ($champ != 0) {
 					?>
 
 					<p>
 						<!-- Formulaire pour l'Update d'un Gisement. -->
 						<form method = "post" action = "../exec/gisementUpdate.php">
 							<p>
-								<label for = "old_nom">Nom</label> : 
-								<select name = "old_nom" id = "old_nom">
-									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_nom"> remplacé par</label> : 
-								<input type = "text" name = "new_nom" id = "new_nom" /><br />
-								<label for = "old_region">Region</label> : 
-								<select name = "old_region" id = "old_region">
-									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom_region'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_region"> remplacé par</label> : 
-								<select name = "new_region" id = "new_region">
-									<option value = "0"></option>
-									<?php
-										$query1->execute();
-										while ($data = $query1->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
-										}
-									?>
-								</select><br />
-								<label for = "old_nord">Position Nord</label> : 
-								<select name = "old_nord" id = "old_nord">
-									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['position_nord'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_nord"> remplacé par</label> : 
-								<input type = "text" name = "new_nord" id = "new_nord" /><br />
-								<label for = "old_est">Position Est</label> : 
-								<select name = "old_est" id = "old_est">
-									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['position_est'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_est"> remplacé par</label> : 
-								<input type = "text" name = "new_est" id = "new_est" /><br />
-								<label for = "altitude">Altitude</label> : 
-								<select name = "old_altitude" id = "old_altitude">
-									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['altitude'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_altitude"> remplacé par</label>
-								<input type = "text" name = "new_altitude" id = "new_altitude" /><br />
-								<label for = "commentaire">Commentaire</label> :<br />
-								<textarea name = "commentaire" id = "commentaire" rows = "5" cols = "40"></textarea><br />
+								<?php if ($champ == "nom") { ?>
+									<label for = "old_nom">Nom</label> : 
+									<select name = "old_nom" id = "old_nom">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_nom"> remplacé par</label> : 
+									<input type = "text" name = "new_nom" id = "new_nom" /><br />
+								<?php } ?>
+								
+								<?php> if ($champ == "region") { ?>
+									<label for = "old_region">Region</label> : 
+									<select name = "old_region" id = "old_region">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['nom_region'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_region"> remplacé par</label> : 
+									<select name = "new_region" id = "new_region">
+										<option value = "0"></option>
+										<?php
+											$query1->execute();
+											while ($data = $query1->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+											}
+										?>
+									</select><br />
+								<?php } ?>
+								
+								<?php> if ($champ == "position_nord") { ?>
+									<label for = "old_nord">Position Nord</label> : 
+									<select name = "old_nord" id = "old_nord">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['position_nord'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_nord"> remplacé par</label> : 
+									<input type = "text" name = "new_nord" id = "new_nord" /><br />
+								<?php } ?>
+								
+								<?php> if ($champ == "position_est") { ?>
+									<label for = "old_est">Position Est</label> : 
+									<select name = "old_est" id = "old_est">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['position_est'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_est"> remplacé par</label> : 
+									<input type = "text" name = "new_est" id = "new_est" /><br />
+								<?php } ?>
+								
+								<?php> if ($champ == "altitude") { ?>
+									<label for = "altitude">Altitude</label> : 
+									<select name = "old_altitude" id = "old_altitude">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['altitude'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_altitude"> remplacé par</label>
+									<input type = "text" name = "new_altitude" id = "new_altitude" /><br />
+								<?php } ?>
+							
 								<input type = "submit" value = "Envoi" />
 							</p>
 						</form>
 					</p>
+					<?php } ?>
 					
 					<?php
-						
+						$query1->closeCursor();
+						$query2->closeCursor();
 					?>
 				
 				</div>
