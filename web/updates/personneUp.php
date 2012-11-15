2c32f2cf4284317d3453996418e7d56c37baf756
antoine <antoine.hars@etu.utc.fr>
2012-11-15 18:35:36 +0100

    Modification du form de PersonneUp.

--- web/updates/personneUp.php
+++ web/updates/personneUp.php
@@ -62,90 +62,131 @@
 
 						/* Récupération des données pour le formulaire. */
 						$query1 = $bdd->prepare('SELECT identifiant, nationalite
-													FROM nationalite'
-													);
+										FROM nationalite'
+										);
 						$query2 = $bdd->prepare('SELECT identifiant, fonction
-													FROM fonction'
-													);
+										FROM fonction'
+										);
 						$query3 = $bdd->prepare('SELECT p.identifiant, p.prenom, p.nom, n.nationalite, f.fonction
-													FROM personne p, nationalite n, fonction f
-													WHERE p.nationalite = n.identifiant
-													AND p.fonction = f.identifiant'
-													);
+										FROM personne p, nationalite n, fonction f
+										WHERE p.nationalite = n.identifiant
+										AND p.fonction = f.identifiant'
+										);
 					?>
 
 					<p>
-						<!-- Formulaire pour l'Update d'une Personne. -->
-						<form method = "post" action = "../exec/personneUpdate.php">
+						<!-- Formulaire sur le choix du champ à modifier. -->
+						<form method = "post" action = "personneUp.php">
 							<p>
-								<label for = "old_nom">Nom</label> : 
-								<select name = "old_nom" id = "old_nom">
-									<option value = "0"></option>4
-									<?php
-										$query3->execute();
-										while ($data = $query3->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_nom"> remplacé par</label> :
-								<input type = "text" name = "new_nom" id = "new_nom" /><br />
-								<label for = "old_prenom">Prénom</label> : 
-								<select name = "old_prenom" id = "old_prenom">
-									<option value = "0"></option>4
-									<?php
-										$query3->execute();
-										while ($data = $query3->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['prenom'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_prenom"> remplacé par</label> : 
-								<input type = "text" name = "new_prenom" id = "new_prenom" /><br />
-								<label for = "old_nationalite">Nationalité</label> : 
-								<select name = "old_nationalite" id = "old_nationalite">
-									<option value = "0"></option>4
-									<?php
-										$query3->execute();
-										while ($data = $query3->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['nationalite'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_nationalite"> remplacé par</label> : 
-								<select name = "new_nationalite" id = "new_nationalite">
-									<option value = "0"></option>4
-									<?php
-										$query1->execute();
-										while ($data = $query1->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['nationalite'] . '</option>';
-										}
-									?>
-								</select><br />
-								<label for = "old_fonction">Fonction</label> : 
-								<select name = "old_fonction" id = "old_fonction">
-									<option value = "0"></option>4
-									<?php
-										$query3->execute();
-										while ($data = $query3->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['fonction'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_fonction"> remplacé par</label> : 
-								<select name = "new_fonction" id = "new_fonction">
-									<option value = "0"></option>4
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['fonction'] . '</option>';
-										}
-									?>
+								<label for = "champ">Champ à modifier</label> : 
+								<select name = "champ" id = "champ">
+									<option value = "0"></option>
+									<option value = "prenom">prénom</option>
+									<option value = "nom">nom</option>
+									<option value = "nationalite">nationalité</option>
+									<option value = "fonction">fonction</option>
 								</select><br />
 								<input type = "submit" value = "Envoi" />
 							</p>
 						</form>
 					</p>
+					
+					<?php
+						/* Récupération du champ à modifier. */
+						$_SESSION['champ'] = 0;
+						if (isset($_POST['champ'])) {	
+							$_SESSION['champ'] = $_POST['champ'];
+						}
+						
+						/* Affichage du champ souhaité. */
+						if ($champ != 0) {
+					?>
+					
+					<p>
+						<!-- Formulaire pour l'Update d'une Personne. -->
+						<form method = "post" action = "../exec/personneUpdate.php">
+							<p>
+								<?php if ($champ == "prenom") { ?>
+									<label for = "old_prenom">Prénom</label> : 
+									<select name = "old_prenom" id = "old_prenom">
+										<option value = "0"></option>4
+										<?php
+											$query3->execute();
+											while ($data = $query3->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['prenom'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_prenom"> remplacé par</label> : 
+									<input type = "text" name = "new_prenom" id = "new_prenom" /><br />
+								<?php } ?>
+								
+								<?php if ($champ == "nom") { ?>
+									<label for = "old_nom">Nom</label> : 
+									<select name = "old_nom" id = "old_nom">
+										<option value = "0"></option>4
+										<?php
+											$query3->execute();
+											while ($data = $query3->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_nom"> remplacé par</label> :
+									<input type = "text" name = "new_nom" id = "new_nom" /><br />
+								<?php } ?>
+								
+								<?php if ($champ == "nationalite") { ?>
+									<label for = "old_nationalite">Nationalité</label> : 
+									<select name = "old_nationalite" id = "old_nationalite">
+										<option value = "0"></option>4
+										<?php
+											$query3->execute();
+											while ($data = $query3->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['nationalite'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_nationalite"> remplacé par</label> : 
+									<select name = "new_nationalite" id = "new_nationalite">
+										<option value = "0"></option>4
+										<?php
+											$query1->execute();
+											while ($data = $query1->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['nationalite'] . '</option>';
+											}
+										?>
+									</select><br />
+								<?php } ?>
+								
+								<?php if ($champ == "fonction") { ?>
+									<label for = "old_fonction">Fonction</label> : 
+									<select name = "old_fonction" id = "old_fonction">
+										<option value = "0"></option>4
+										<?php
+											$query3->execute();
+											while ($data = $query3->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['fonction'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_fonction"> remplacé par</label> : 
+									<select name = "new_fonction" id = "new_fonction">
+										<option value = "0"></option>4
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['fonction'] . '</option>';
+											}
+										?>
+									</select><br />
+								<?php } ?>
+							
+								<input type = "submit" value = "Envoi" />
+							</p>
+						</form>
+					</p>
+					<?php } ?>
 					
 					<?php
 						$query1->closeCursor();
