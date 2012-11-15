25a4e94114d7d54c5c1c36e8652a44aef6bec657
antoine <antoine.hars@etu.utc.fr>
2012-11-15 13:29:46 +0100

    Modification du form de CharbonUp.

--- web/updates/charbonUp.php
+++ web/updates/charbonUp.php
@@ -62,56 +62,88 @@
 
 						/* Récupération des données pour le formulaire. */
 						$query1 = $bdd->prepare('SELECT o.identifiant, o.nom
-																			FROM objet o, objetnature n
-																			WHERE o.nature = n.identifiant
-																			AND n.nature = \'charbon\''
-																		 	);
+										FROM objet o, objetnature n
+										WHERE o.nature = n.identifiant
+										AND n.nature = \'charbon\''
+									 	);
 						$query2 = $bdd->prepare('SELECT o.nom AS nom_objet, c.datation
-																			FROM charbon c, objet o
-																			WHERE c.objet = o.identifiant'
-																			);
+										FROM charbon c, objet o
+										WHERE c.objet = o.identifiant'
+										);
 					?>
 
 					<p>
-						<!-- Formulaire pour l'Update d'un Charbon. -->
-						<form method = "post" action = "../exec/charbonUpdate.php">
+						<form method = "post" action = "charbonUp.php">
 							<p>
-								<label for = "old_objet">Objet</label> : 
-								<select name = "old_objet" id = "old_objet">
+								<label for = "champ">Champ à modifier</label> : 
+								<select name = "champ" id = "champ">
 									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom_objet'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_objet"> remplacé par</label> : 
-								<select name = "new_objet" id = "new_objet">
-									<option value = "0"></option>
-									<?php
-										$query1->execute();
-										while ($data = $query1->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
-										}
-									?>
+									<option value = "objet">objet</option>
+									<option value = "datation">datation</option>
 								</select><br />
-								<label for = "old_datation">Datation</label> : 
-								<select name = "old_datation" id = "old_datation">
-									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value ="' . $data['identifiant'] . '">' . $data['datation'] . '</option>';
-										}
-									?>
-								</select>
-								<input type = "text" name = "new_datation" id = "new_datation" /><br />
 								<input type = "submit" value = "Envoi" />
 							</p>
 						</form>
 					</p>
 					
+					<?php
+						/* Récupération du champ à modifier. */
+						$_SESSION['champ'] = 0;
+						if (isset($_POST['champ'])) {	
+							$_SESSION['champ'] = $_POST['champ'];
+						}
+			
+						/* Affichage du champ souhaité. */
+						if ($champ != 0) {
+					?>
+
+					<p>
+						/* Formulaire pour l'Update d'un charbon. */
+						<form method = "post" action = "../exec/charbonUpdate.php">
+							<p>
+								<?php if ($champ == "objet") { ?>
+									<label for = "old_objet">Objet</label> : 
+									<select name = "old_objet" id = "old_objet">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['nom_objet'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_objet"> remplacé par</label> : 
+									<select name = "new_objet" id = "new_objet">
+										<option value = "0"></option>
+										<?php
+											$query1->execute();
+											while ($data = $query1->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+											}
+										?>
+									</select><br />
+								<?php } ?>
+			
+								<?php> if ($champ == "datation") { ?>
+									<label for = "old_datation">Datation</label> : 
+									<select name = "old_datation" id = "old_datation">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value ="' . $data['identifiant'] . '">' . $data['datation'] . '</option>';
+											}
+										?>
+									</select>
+									<input type = "text" name = "new_datation" id = "new_datation" /><br />
+								<?php } ?>
+		
+								<input type = "submit" value = "Envoi" />
+							</p>
+						</form>
+					</p>
+					<?php } ?>
+
 					<?php
 						$query1->closeCursor();
 						$query2->closeCursor();
