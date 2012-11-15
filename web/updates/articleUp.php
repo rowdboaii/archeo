8c6ce2e101d47c7b195ef3fbebaa7ae72690a219
antoine <antoine.hars@etu.utc.fr>
2012-11-15 13:07:27 +0100

    Modification du form de ArticleUp.

--- web/updates/articleUp.php
+++ web/updates/articleUp.php
@@ -1,18 +1,18 @@
-<!-- Sujet : Projet de base de donnÈes pour des fouilles archÈologiques. -->
+<!-- Sujet : Projet de base de donn√©es pour des fouilles arch√©ologiques. -->
 <!-- Auteur : Xavier Muth & Antoine Hars -->
 <!-- Fichier : articleUp.php -->
 
-<!-- DÈmarrage de la session pour les identifiants. -->
+<!-- D√©marrage de la session pour les identifiants. -->
 <?php session_start(); ?>
 
 <!DOCTYPE html>
 <html>
 
 	<head>
-		<!-- En-tÍte de la page. -->
+		<!-- En-t√™te de la page. -->
 		<meta charset = "utf-8" />
 		<link rel = "stylesheet" href = "style.css" />
-		<!-- Dans le cas o˘ le navigateur est une version antÈrieure ‡ IE9 -->
+		<!-- Dans le cas o√π le navigateur est une version ant√©rieure √† IE9 -->
 		<!--[if lt IE9]>
 			<script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
@@ -41,7 +41,7 @@
 			</nav>
 			
 			<aside>
-				<!-- Menu latÈral spÈcifique au lien visitÈ. -->
+				<!-- Menu lat√©ral sp√©cifique au lien visit√©. -->
 				<div id = "">
 				
 					<!-- Menu pour les Updates. -->
@@ -57,30 +57,59 @@
 				<div id = "">
 
 					<?php
-						/* Connexion ‡ la base de donnÈes. */
+						/* Connexion √† la base de donn√©es. */
 						include('../includes/connexionBDD.php');
 
-						/* RÈcupÈration des donnÈes pour le formulaire. */
+						/* R√©cup√©ration des donn√©es pour le formulaire. */
 						$query1 = $bdd->prepare('SELECT identifiant, prenom, nom
-																			FROM personne'
-																			);
+										FROM personne'
+										);
 						$query2 = $bdd->prepare('SELECT identifiant, nom
-																			FROM locus'
-																			);
+										FROM locus'
+										);
 						$query3 = $bdd->prepare('SELECT identifiant, langue
-																			FROM langue'
-																			);
+										FROM langue'
+										);
 						$query4 = $bdd->prepare('SELECT a.identifiant, a.titre, a.auteur, p.prenom, p.nom, a.mot_cle, a.annee, a.revue, a.sujet, l.langue AS nom_langue
-																			FROM article a, personne p, langue l
-																			WHERE a.auteur = p.identifiant
-																			AND a.langue = l.identifiant'
-																			);
+										FROM article a, personne p, langue l
+										WHERE a.auteur = p.identifiant
+										AND a.langue = l.identifiant'
+										);
 					?>
 
 					<p>
-						<!-- Formulaire pour l'Update d'un Article. -->
-						<form method = "post" action = "../exec/articleUpdate.php">
+						<form method = "post" action = "articleUp.php">
 							<p>
+								<label for = "champ">Champ √† modifier</label> : 
+								<select name = "champ" id = "champ">
+									<option value = "0"></option>
+									<option value = "titre">titre</option>
+									<option value = "auteur">auteur</option>
+									<option value = "annee">ann√©e</option>
+									<option value = "revue">revue</option>
+									<option value = "sujet">sujet</option>
+									<option value = "langue">langue</option>
+								</select><br />
+								<input type = "submit" value = "Envoi" />
+							</p>
+						</form>
+					</p>
+
+					<?php
+						/* R√©cup√©ration du champ √† modifier. */
+						$_SESSION['champ'] = 0;
+						if (isset($_POST['champ'])) {	
+							$_SESSION['champ'] = $_POST['champ'];
+						}
+	
+						/* Affichage du champ souhait√©. */
+						if ($champ != 0) {
+					?>
+					
+					/* Formulaire pour l'Update d'un article. */
+					<form method = "post" action = "../exec/articleUpdate.php">
+						<p>
+							<?php if ($champ == "titre") { ?>
 								<label for = "old_titre">Titre</label> : 
 								<select name = "old_titre" id = "old_titre">
 									<option value = "0"></option>
@@ -91,8 +120,11 @@
 										}
 									?>
 								</select>
-								<label for = "new_titre"> remplacÈ par</label> :
+								<label for = "new_titre"> remplac√© par</label> :
 								<input type = "text" name = "new_titre" id = "new_titre" /><br />
+							<?php } ?>
+				
+							<?php> if ($champ == "auteur") { ?>
 								<label for = "old_auteur">Auteur</label> : 
 								<select name = "auteur" id = "auteur">
 									<option value = "0"></option>
@@ -103,7 +135,7 @@
 										}
 									?>
 								</select>
-								<label for = "new_auteur"> remplacÈ par</label> : 
+								<label for = "new_auteur"> remplac√© par</label> : 
 								<select name = "new_auteur" id = "new_auteur">
 									<option value = "0"></option>
 									<?php
@@ -113,39 +145,10 @@
 										}
 									?>
 								</select><br />
-								<label for = "old_revue">Revue</label> : 
-								<select name = "old_revue" id = "old_revue">
-									<option value = "0"></option>
-									<?php
-										$query4->execute();
-										while ($data = $query4->fetch()) {
-											echo '<option value = "' . $data['identifiant'] . '">' . $data['revue'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_revue"> remplacÈ par</label> : 
-								<input type = "text" name = "revue" id = "revue" /><br />
-								<label for = "old_sujet">Sujet</label> : 
-								<select name = "old_sujet" id = "old_sujet">
-									<option value = "0"></option>
-									<?php
-										$query4->execute();
-										while ($data = $query4->fetch()) {
-											echo '<option value = "' . $data['identifiant'] . '">' . $data['sujet'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_sujet"> remplacÈ par</label> : 
-								<select name = "new_sujet" id = "new_sujet">
-									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
-										}
-									?>
-								</select><br />
-								<label for = "old_annee">AnnÈe</label> : 
+							<?php } ?>
+				
+							<?php if ($champ == "annee") { ?>
+								<label for = "old_annee">Ann√©e</label> : 
 								<select name = "old_annee" id = "old_annee">
 									<option value = "0"></option>
 									<?php
@@ -156,6 +159,47 @@
 									?>
 								</select>
 								<input type = "text" name = "new_annee" id = "new_annee" /><br />
+							<?php } ?>
+				
+							<?php if ($champ == "revue") { ?>
+								<label for = "old_revue">Revue</label> : 
+								<select name = "old_revue" id = "old_revue">
+									<option value = "0"></option>
+									<?php
+										$query4->execute();
+										while ($data = $query4->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['revue'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_revue"> remplac√© par</label> : 
+								<input type = "text" name = "revue" id = "revue" /><br />
+							<?php } ?>
+						
+							<?php if ($champ == "sujet") { ?>
+								<label for = "old_sujet">Sujet</label> : 
+								<select name = "old_sujet" id = "old_sujet">
+									<option value = "0"></option>
+									<?php
+										$query4->execute();
+										while ($data = $query4->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['sujet'] . '</option>';
+										}
+									?>
+								</select>
+								<label for = "new_sujet"> remplac√© par</label> : 
+								<select name = "new_sujet" id = "new_sujet">
+									<option value = "0"></option>
+									<?php
+										$query2->execute();
+										while ($data = $query2->fetch()) {
+											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+										}
+									?>
+								</select><br />
+							<?php } ?>
+					
+							<?php if ($champ == "langue") { ?>
 								<label for = "old_langue">Langue</label> : 
 								<select name = "old_langue" id = "old_langue">
 									<option value = "0"></option>
@@ -166,9 +210,8 @@
 										}
 									?>
 								</select>
-								<label for = "new_langue"> remplacÈ par</label> : 
+								<label for = "new_langue"> remplac√© par</label> : 
 								<select name = "new_langue" id = "new_langue">
-									<option value = "0"></option>
 									<option value = "0"></option>
 									<?php
 										$query3->execute();
@@ -177,11 +220,14 @@
 										}
 									?>
 								</select><br />
-								<input type = "submit" value = "Envoi" />
-							</p>
-						</form>
-					</p>
-					
+							<?php } ?>
+			
+							<input type = "submit" value = "Envoi" />
+						</p>
+					</form>
+				
+					<?php } ?>
+
 					<?php
 						$query1->closeCursor();
 						$query2->closeCursor();
