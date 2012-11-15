dd45847004134a6db02a56ec6663c1112c03c335
antoine <antoine.hars@etu.utc.fr>
2012-11-15 18:43:51 +0100

    Modification du form de RégionUp.

--- web/updates/regionUp.php
+++ web/updates/regionUp.php
@@ -62,54 +62,87 @@
 
 						/* Répération des données pour le formulaire. */
 						$query1 = $bdd->prepare('SELECT identifiant, nom
-													FROM pays'
-												 	);
+										FROM pays'
+									 	);
 						$query2 = $bdd->prepare('SELECT r.identifiant, r.nom, p.nom AS nom_pays
-													FROM region r, pays p
-													WHERE r.pays = p.identifiant'
-													);
+										FROM region r, pays p
+										WHERE r.pays = p.identifiant'
+										);
 					?>
 
 					<p>
-						<!-- Formulaire pour l'Update d'une Région. -->
-						<form method = "post" action = "../exec/regionUpdate.php">
+						<!-- Formulaire sur le choix du champ à modifier. -->
+						<form method = "post" action = "regionUp.php">
 							<p>
-								<label for = "old_nom">Nom</label> : 
-								<select name = "old_nom" id = "old_nom">
+								<label for = "champ">Champ à modifier</label> : 
+								<select name = "champ" id = "champ">
 									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_nom"> remplacé par</label> : 
-								<input type = "text" name = "new_nom" id = "new_nom" /><br />
-								<label for = "old_pays">Pays</label> : 
-								<select name = "old_pays" id = "old_pays">
-									<option value = "0"></option>
-									<?php
-										$query2->execute();
-										while ($data = $query2->fetch()) {
-											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom_pays'] . '</option>';
-										}
-									?>
-								</select>
-								<label for = "new_nom"> remplacé par</label> : 
-								<select name = "new_pays" id = "new_pays">
-									<option value = "0"></option>
-									<?php
-										$query1->execute();
-										while ($data = $query1->fetch()) {
-											echo '<option value = "' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
-										}
-									?>
+									<option value = "nom">nom</option>
+									<option value = "pays">pays</option>
 								</select><br />
 								<input type = "submit" value = "Envoi" />
 							</p>
 						</form>
 					</p>
+					
+					<?php
+						/* Récupération du champ à modifier. */
+						$_SESSION['champ'] = 0;
+						if (isset($_POST['champ'])) {	
+							$_SESSION['champ'] = $_POST['champ'];
+						}
+						
+						/* Affichage du champ souhaité. */
+						if ($champ != 0) {
+					?>
+					
+					<p>
+						<!-- Formulaire pour l'Update d'une Région. -->
+						<form method = "post" action = "../exec/regionUpdate.php">
+							<p>
+								<?php if ($champ == "nom") { ?>
+									<label for = "old_nom">Nom</label> : 
+									<select name = "old_nom" id = "old_nom">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value = "' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_nom"> remplacé par</label> : 
+									<input type = "text" name = "new_nom" id = "new_nom" /><br />
+								<?php } ?>
+								
+								<?php if ($champ == "pays") { ?>
+									<label for = "old_pays">Pays</label> : 
+									<select name = "old_pays" id = "old_pays">
+										<option value = "0"></option>
+										<?php
+											$query2->execute();
+											while ($data = $query2->fetch()) {
+												echo '<option value = "' . $data['identifiant'] . '">' . $data['nom_pays'] . '</option>';
+											}
+										?>
+									</select>
+									<label for = "new_pays"> remplacé par</label> : 
+									<select name = "new_pays" id = "new_pays">
+										<option value = "0"></option>
+										<?php
+											$query1->execute();
+											while ($data = $query1->fetch()) {
+												echo '<option value = "' . $data['identifiant'] . '">' . $data['nom'] . '</option>';
+											}
+										?>
+									</select><br />
+								<?php } ?>
+							
+								<input type = "submit" value = "Envoi" />
+							</p>
+						</form>
+					</p>
+					<?php } ?>
 					
 					<?php
 						$query1->closeCursor();
