867ceff9d618cf45bab11bee2a1e82754cca6600
antoine <antoine.hars@etu.utc.fr>
2012-11-19 19:05:34 +0100

    Update web/exec/fonctionUpdate.php

--- web/exec/fonctionUpdate.php
+++ web/exec/fonctionUpdate.php
@@ -53,12 +53,12 @@
 
 					<?php
 						$query = $bdd->prepare('UPDATE fonction
-												SET fonction = :new
-												WHERE fonction = :old'
-												);
+									SET fonction = :new
+									WHERE fonction = :old'
+									);
 						$query->execute(array('new' => $_POST['new'],
-												'old' => $_POST['old']
-												));
+									'old' => $_POST['old']
+									));
 
 						if (!$query) {
 							die("Erreur dans l'insertion : " . pg_last_error());
