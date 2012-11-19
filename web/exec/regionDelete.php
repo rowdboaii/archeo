1cced6a29a4f7b3a284a85a9867a5747ca74cad1
antoine <antoine.hars@etu.utc.fr>
2012-11-19 19:18:30 +0100

    Update web/exec/regionDelete.php

--- web/exec/regionDelete.php
+++ web/exec/regionDelete.php
@@ -54,7 +54,7 @@
 					<?php
 						$query = $bdd->prepare('DELETE FROM region
 									WHERE identifiant = :delete'
-												);
+									);
 						$query->execute(array('delete' => $_POST['delete']));
 
 						if (!$query) {
