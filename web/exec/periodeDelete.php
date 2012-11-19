a8a36871d6d68cddffcf639dc3a4999eb2f7e102
antoine <antoine.hars@etu.utc.fr>
2012-11-19 18:28:33 +0100

    Update web/exec/periodeDelete.php

--- web/exec/periodeDelete.php
+++ web/exec/periodeDelete.php
@@ -53,8 +53,8 @@
 
 					<?php
 						$query = $bdd->prepare('DELETE FROM periode
-												WHERE periode = :delete'
-												);
+									WHERE periode = :delete'
+									);
 						$query->execute(array('delete' => $_POST['delete']));
 
 						if (!$query) {
