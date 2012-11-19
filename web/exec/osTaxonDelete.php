7edd4cae42d6ef66b07582e25743eedc9d547843
antoine <antoine.hars@etu.utc.fr>
2012-11-19 18:36:19 +0100

    Update web/exec/osTaxonDelete.php

--- web/exec/osTaxonDelete.php
+++ web/exec/osTaxonDelete.php
@@ -53,8 +53,8 @@
 
 					<?php
 						$query = $bdd->prepare('DELETE FROM ostaxon
-												WHERE taxon = :delete'
-												);
+									WHERE taxon = :delete'
+									);
 						$query->execute(array('delete' => $_POST['delete']));
 
 						if (!$query) {
