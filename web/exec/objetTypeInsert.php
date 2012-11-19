d52a47fd2fafa03086d1f1baff2957a2982c13c9
antoine <antoine.hars@etu.utc.fr>
2012-11-19 18:42:12 +0100

    Update web/exec/objetTypeInsert.php

--- web/exec/objetTypeInsert.php
+++ web/exec/objetTypeInsert.php
@@ -53,7 +53,7 @@
 	
 					<?php
 						$query = $bdd->prepare('INSERT INTO objettype (type)
-												VALUES (:type)');
+									VALUES (:type)');
 						$query->execute(array('type' => $_POST['type']));
 
 						if (!$query) {
