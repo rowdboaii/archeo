d02e79c7babf1733353595caad7ac894dac4e407
antoine <antoine.hars@etu.utc.fr>
2012-11-19 18:26:57 +0100

    Update web/exec/personneInsert.php

--- web/exec/personneInsert.php
+++ web/exec/personneInsert.php
@@ -53,12 +53,12 @@
 	
 					<?php
 						$query = $bdd->prepare('INSERT INTO personne (nom, prenom, nationalite, fonction)
-												VALUES (:nom, :prenom, :nationalite, :fonction)');
+									VALUES (:nom, :prenom, :nationalite, :fonction)');
 						$query->execute(array('nom' => $_POST['nom'],
-												'prenom' => $_POST['prenom'],
-												'nationalite' => $_POST['nationalite'],
-												'fonction' => $_POST['fonction']
-												)) or die('Error');
+									'prenom' => $_POST['prenom'],
+									'nationalite' => $_POST['nationalite'],
+									'fonction' => $_POST['fonction']
+								)) or die('Error');
 						echo 'Champ ajouté à la base.';
 					?>
 				
