d205ab6b1ee216e5d4c65982106cb92f3fb0b88c
antoine <antoine.hars@etu.utc.fr>
2012-11-19 18:55:37 +0100

    Update web/exec/langueDelete.php

--- web/exec/langueDelete.php
+++ web/exec/langueDelete.php
@@ -53,8 +53,8 @@
 
 					<?php
 						$query = $bdd->prepare('DELETE FROM langue
-												WHERE langue = :delete'
-												);
+									WHERE langue = :delete'
+									);
 						$query->execute(array('delete' => $_POST['delete']));
 
 						if (!$query) {
