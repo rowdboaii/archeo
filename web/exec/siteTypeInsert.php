78b41ec480969f19ccf7b574482f4407a3c01dab
antoine <antoine.hars@etu.utc.fr>
2012-11-19 19:17:08 +0100

    Update web/exec/siteTypeInsert.php

--- web/exec/siteTypeInsert.php
+++ web/exec/siteTypeInsert.php
@@ -53,7 +53,8 @@
 	
 					<?php
 						$query = $bdd->prepare('INSERT INTO sitetype (type)
-									VALUES (:type)');
+									VALUES (:type)'
+									);
 						$query->execute(array('type' => $_POST['type']));
 
 						if (!$query) {
